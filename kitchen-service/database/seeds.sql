afdcd7c7c17d36880f9ddc7a7d8e89309eb88e03
Gabriel Guerra <gfranklings@gmail.com>
2024-12-21 22:56:08 -0400

    chore: Commenting selects on seeds.sql

--- kitchen-service/database/seeds.sql
+++ kitchen-service/database/seeds.sql
@@ -26,18 +26,18 @@ INSERT INTO prepared_food_ingredients (id_prepared_food, id_ingredient, required
 (3, 8, 2),  -- 2 de mayonesa para el pan simple
 (3, 9, 1);  -- 1 pan para el pan simple
 
-SELECT * FROM ingredients;
-SELECT * FROM prepared_foods;
+-- SELECT * FROM ingredients;
+-- SELECT * FROM prepared_foods;
 
-SELECT
-    ing.name AS ingredient,
-    pfi.required_amount
-FROM
-    prepared_food_ingredients pfi
-    INNER JOIN prepared_foods pf ON pf.id = pfi.id_prepared_food
-    INNER JOIN ingredients ing ON ing.id = pfi.id_ingredient
-WHERE
-    pf.id = 1;
+-- SELECT
+--     ing.name AS ingredient,
+--     pfi.required_amount
+-- FROM
+--     prepared_food_ingredients pfi
+--     INNER JOIN prepared_foods pf ON pf.id = pfi.id_prepared_food
+--     INNER JOIN ingredients ing ON ing.id = pfi.id_ingredient
+-- WHERE
+--     pf.id = 1;
 
 -- UPDATE ingredients SET amount = 1 WHERE id = 2;
 -- UPDATE prepared_food_ingredients SET required_amount = 4 WHERE id = 2;
--- warehouse-service/database/seeds.sql
+++ warehouse-service/database/seeds.sql
@@ -2,4 +2,4 @@ INSERT INTO products (name, amount) VALUES
 ('lechuga', 2),
 ('pollo', 4);
 
-SELECT * FROM products;
+-- SELECT * FROM products;
